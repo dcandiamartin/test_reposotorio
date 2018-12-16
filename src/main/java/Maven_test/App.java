5293dc6976c86594e721f0c360a8574a2b362640
David Candia <x3455667767@192.168.1.20>
2018-12-16 17:03:34 +0100

    Adición de ficheros al index

---
+++ src/main/java/Maven_test/App.java
@@ -0,0 +1,13 @@
+package Maven_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+	    // Comentario de prueba en main 
+		Test t = new Test();
+		t.pruebaJerarquia();
+	}
+}
---
+++ src/main/java/Maven_test/Test.java
@@ -0,0 +1,34 @@
+package Maven_test;
+
+public class Test {
+	class A{
+		int i = 10;
+	}
+	class B extends A{
+		int j = 20;
+	}
+	class C extends B{
+		int k = 30;
+	}
+
+	public void pruebaJerarquia() {
+//		A a = new B();
+//		B b = (B)a;
+//		C c = (C)b;
+		
+//		You are creating one object of type B. As per your class hierarchy, each B object is also an A, but not a C.
+//		Now let's look on how to assign the reference to this B object to various variables:
+		// Mensaje de prueba escrito en repositorio remoto
+//		B b = (B)new A(); 
+		// texto de prueba en clase Test
+		/*
+		Object o = new B(); // You can always assign references to an Object-typed variable.
+		A a = (A) o; // This works because the object is of type A.
+		B b = (B) o; // This works because the object is of type B.
+		C c = (C) o; // This does not work because the object is not of type C.
+		*/
+//		System.out.println(c.k);
+		// comentario de prueba
+		System.out.println("Texto de prueba");
+	}
+}
